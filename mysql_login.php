a2b8c9e41528f10a8f3e9d8fca7da72242a88379
jonathan <jonathan.dorado@nuevosmedios.ws>
2016-05-29 21:00:04 -0500

    edwin hizo mal el prueto

--- mysql_login.php
+++ mysql_login.php
@@ -3,8 +3,8 @@
  * Provee las constantes para conectarse a la base de datos
  * Mysql.
  */
-define("HOSTNAME", "localhost");// Nombre del host
-define("DATABASE", "mydb"); // Nombre de la base de datos
-define("USERNAME", "root"); // Nombre del usuario
-define("PASSWORD", ""); // Nombre de la constraseña
+define("HOSTNAME", "us-cdbr-azure-southcentral-e.cloudapp.net");// Nombre del host
+define("DATABASE", "canchaDB"); // Nombre de la base de datos
+define("USERNAME", "b209954b19644e"); // Nombre del usuario
+define("PASSWORD", "72ebcdbc"); // Nombre de la constraseña
 ?>
