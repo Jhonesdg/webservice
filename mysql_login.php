78c1b87fe9db4af4ac6592bdcaf55ca4129de610
jonathan <jonathan.dorado@nuevosmedios.ws>
2016-05-29 22:08:52 -0500

    jkd5

--- mysql_login.php
+++ mysql_login.php
@@ -4,7 +4,7 @@
  * Mysql.
  */
 define("HOSTNAME", "us-cdbr-azure-southcentral-e.cloudapp.net");// Nombre del host
-define("DATABASE", "canchaDB"); // Nombre de la base de datos
+define("DATABASE", "canchadb"); // Nombre de la base de datos
 define("USERNAME", "b209954b19644e"); // Nombre del usuario
 define("PASSWORD", "72ebcdbc"); // Nombre de la constraseña
 ?>
